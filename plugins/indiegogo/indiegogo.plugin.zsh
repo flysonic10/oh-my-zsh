7cc8d4a625ba6034015bc120dc5c168df368e741
William Wnekowicz <w@indiegogo.com>
2016-02-12 09:29:18 -0800

    Add various aliases

--- plugins/indiegogo/indiegogo.plugin.zsh
+++ plugins/indiegogo/indiegogo.plugin.zsh
@@ -9,14 +9,23 @@ alias rdm='rake db:migrate'
 alias rdt='rake db:test:prepare'
 alias ra='b && r18 && rdm && rdt'
 
-alias grsv='grb serenity/valley'
-alias gcosv='gco serenity/valley'
-alias gmsv='gm serenity/valley'
+alias grbsv='grb time/machine'
+alias gcosv='gco time/machine'
+alias grbtm='grb time/machine'
+alias gcotm='gco time/machine'
+alias gmsv='gm time/machine'
+alias gmtm='gm time/machine'
 alias gr^='git reset HEAD^'
 alias gv='gco vendor/assets/ng-gogo/version.txt --theirs'
 alias gsiu='git stash --include-untracked'
 alias gcoh='git reflog|grep checkout: | head -n 15'
-gcos() { gco -b serenity/"$*"; }
+gcos() { gco serenity/"$*"; }
+gcobs() { gco -b serenity/"$*"; }
+gcot() { gco time/"$*"; }
+gcobt() { gco -b time/"$*"; }
+
+alias ng='c ng-gogo'
+ngsha () { echo "[ng-gogo: `ng && git rev-parse --short head`]" }
 
 alias ks='karma start --single-run'
 alias fs='foreman start'
@@ -26,8 +35,10 @@ alias gw='gulp watch'
 alias gbld='gulp build'
 
 alias pr='hub pull-request'
-alias psv='hub pull-request -b serenity/valley'
-alias prsv='hub pull-request -b serenity/valley'
+alias psv='hub pull-request -b time/machine'
+alias prsv='hub pull-request -b time/machine'
+alias ptm='hub pull-request -b time/machine'
+alias prtm='hub pull-request -b time/machine'
 alias hbm='hub browse indiegogo/monorail'
 alias hbn='hub browse indiegogo/ng-gogo'
 alias hbh='hub browse indiegogo/harvest'
